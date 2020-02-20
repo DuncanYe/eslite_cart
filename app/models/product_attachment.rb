9967efedc0b20220f217b0d648068f81c9f14bf8
Duncan <loxa10077@gmail.com>
2020-02-20 22:27:11 +0800

    create product_attachment model

---
+++ app/models/product_attachment.rb
@@ -0,0 +1,5 @@
+class ProductAttachment < ApplicationRecord
+  mount_uploader :attachment, ProductAttachmentUploader
+  belongs_to :product
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_054024) do
+ActiveRecord::Schema.define(version: 2020_02_20_125515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "product_attachments", force: :cascade do |t|
+    t.string "attachment"
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_product_attachments_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -41,4 +49,5 @@ ActiveRecord::Schema.define(version: 2020_02_20_054024) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "product_attachments", "products"
 end
