429c56a21b5b780b4e89b74903725f6d0d32d781
Duncan <loxa10077@gmail.com>
2020-02-20 15:09:53 +0800

    add i18n and add validation to product
    add check_product_price method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,17 @@
 class Product < ApplicationRecord
   scope :avalible_products, -> { where(state: ["on_sale", "purchasing"])}
+  validates_numericality_of :original_price, :sell_price, :sku, only_integer: true, greater_than_or_equal_to: 0
+  validate :check_product_price
 
   enum state: {
     on_sale: "on_sale",
-    off: "off",
+    discontinued: "discontinued",
     purchasing: "purchasing"
   }
+
+  def check_product_price
+    if sell_price > original_price
+      errors.add(:sell_price, "售價不能高於原價")
+    end
+  end
 end
