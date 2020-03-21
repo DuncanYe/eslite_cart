5079b53bedd5be6ceee3c31e5315efbdfc063baf
Duncan <loxa10077@gmail.com>
2020-03-21 21:28:19 +0800

    use cnacnacan gem to authorize admin

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,13 +1,11 @@
 class Admin::BaseController < ApplicationController
-  before_action :authenticate_admin
-  private
 
-   def authenticate_admin
-     unless current_user.admin?
-       flash[:alert] = "What?"
-       redirect_to root_path
-     end
-   end
-
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, notice: exception.message }
+      format.js   { head :forbidden, content_type: 'text/html' }
+    end
+  end
 
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < Admin::BaseController
   before_action :find_product, only: [:edit, :update, :destroy]
+  load_and_authorize_resource
 
   def index
     @products = Product.all
